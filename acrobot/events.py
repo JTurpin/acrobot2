4b0ff3f4b037d692d55488147845042abdedeb51
James Fefes <james.fefes@uplight.com>
2020-01-19 21:26:11 -0700

    Fixed typo in help prompt

--- acrobot/events.py
+++ acrobot/events.py
@@ -31,7 +31,7 @@ def build_acronym_response(found_acronyms):
     # separated into its own method so that this can be a more robust and reusable response in the future
     found_count = len(found_acronyms)
     if found_count < 1:
-        return "Couldn't find any matches. You can now add new acronyms with `/acrobot-add /\"acronym\" \"definition\". Try it yourself!`"
+        return "Couldn't find any matches. You can now add new acronyms with `/acrobot-add \"acronym\" \"definition\". Try it yourself!`"
 
     acronym_definitions = ", or ".join([acronym.acronym_definition for acronym in found_acronyms])
     message = f"Found {found_count} possible result(s): {acronym_definitions}"
